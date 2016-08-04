1606b4895a851899759e273ae0f80dca14bd1c17
Sierra Brown <sbrown@23andme.com>
2016-08-03 23:56:06 -0700

    start family tree jsx

--- react-pedigree/js/Index.jsx
+++ react-pedigree/js/Index.jsx
@@ -4,39 +4,96 @@ import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
 
 import {ReduxRouter} from 'redux-router';
-// import configureStore from './store/configureStore';
+import {_} from 'underscore';
 
-// const store = configureStore();
-// const AppRoot = require('./AppRoot');
+class FamilyTree extends React.Component {
 
-// class RootElement extends React.Component {
-// 	static displayName = 'RootElement';
-// 	render() {
-// 		return (
-// 			<Provider store={store}>
-// 				<ReduxRouter routes = {AppRoot} />
-// 			</Provider>
-// 		);
-// 	}
-// }
+	constructor(props) {
+		super(props)
+		this.state = {
+			'nodes': this.props.nodes,
+		}
+		this._renderNode = this._renderNode.bind(this);
+	}
 
-// ReactDom.render(
-// 	<RootElement />,
-// 	document.getElementById('react-app')
-// );
-
-class HelloWorld extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Hello, World!</h1>
+				{ this._renderNodes(this.props.parentNode) }
+			</div>
+		)
+	}
+
+	_renderNodes(current_id) {
+		return this._renderNode(current_id);
+	}
+
+	_renderNode(current_id) {
+		var current_node = _.find(this.state.nodes, function(x) {
+			if (x.id === current_id) {
+				return x
+			};
+		}, this);
+		if (current_node.children.length < 1) {
+			return (
+				<Node
+					key={current_node.id}
+					name={current_node.name} 
+				/>
+			);
+		} else {
+			var children = current_node.children.map(
+				function(node) {
+					return this._renderNode(node)
+				},
+				this
+			)
+			return (
+				<Node
+					key={current_node.id}
+					name={current_node.name} 
+				/>
+				children[0]
+			)
+		}
+	}
+}
+
+class Node extends React.Component {
+	constructor(props) {
+		super(props)
+		this.setState({
+			'parent': props.parent,
+			'children': props.children,
+			'name': props.name
+		})
+	}
+	render() {
+		return (
+			<div class="node">
+				<span><strong>{this.props.name}</strong></span>
 			</div>
 		)
 	}
 }
 
+var data = [
+{
+	'parent': null, 
+	'children': [2], 
+	'name': 'Joey',
+	'id': 1
+},
+{
+	'parent': 1, 
+	'children': [], 
+	'name': 'Monica',
+	'id': 2
+}]
+
+var parentNode = 1;
 
 ReactDom.render(
-	<HelloWorld />,
+	<FamilyTree nodes={data} parentNode={parentNode}/>,
 	document.getElementById('react-app')
 );
